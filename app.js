a84f6fcaf770a93c7a06cb33208b1fba05adcf54
ChrisBoisjoli <christopherboisjoli@gmail.com>
2021-02-23 17:42:19 -0600

    added console log to prompt build team

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+// const validator = require("email-validator");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -14,12 +15,8 @@ const render = require("./lib/htmlRenderer");
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-//Please Build your Team
 
-//manager's name
-//manager ID
-//manager email force valid email
-//Manager office number
+
 
 //build team select engineer or intern
 
@@ -40,7 +37,12 @@ const render = require("./lib/htmlRenderer");
 
 
 
+//Please Build your Team
+console.log('Please Build Your Team'),
+
 inquirer.prompt([
+   
+    
     {//manager's name
         type: 'input',
         message: 'What is your Managers name?',
@@ -60,9 +62,13 @@ inquirer.prompt([
         type: 'input',
         message: 'What is your Managers email?',
         name: 'email',
-    }
+        // validate: 'email','email': "required|email", 
+    },
 
 ]).then((response) => console.log(response) );
+
+// render();
+
 
 
 // After the user has input all employees desired, call the `render` function (required
